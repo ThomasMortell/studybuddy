12853170b17eb6e5aefa0bf18267e306671ccf0d
Guoda <33546350+guodap@users.noreply.github.com>
2019-11-24 21:57:35 +0000

    Update groups.js
    Uncommented the group search returned code and added a card

---
+++ src/store/actions/groups.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import firebase from "../../services/firebase.js";
+
+var db = firebase.firestore();
+
+export const createGroup = () => {
+		const GroupCollection = db.collection('groups');
+		GroupCollection.doc(document.getElementById("cgroupName").value).set({
+		ModuleCode:document.getElementById("cgroupModuleCode").value,
+		Timetable: "",
+		User0: "",
+		User1: "",
+		User2: "",
+		User3: "",
+		User4: "",
+		User5: "",
+		User6: "",
+		User7: "",
+		User8: "",
+		User9: "",
+	})
+}
+
+export const searchGroups = () => {
+	if(document.getElementById("groupSearch").value == ""){
+		document.getElementById("groupDisplay").innerHTML = "Please enter a value and try again.";
+	}
+	else{
+		let GroupCollection = db.collection('groups').doc(document.getElementById("groupSearch").value);
+		GroupCollection.get()
+		  .then(doc => {
+				if (!doc.exists) {
+			  	document.getElementById("groupDisplay").innerHTML = "No Results.";
+				} else {
+			  	//var bane = document.createElement('button').bane.onClick = joinGroup();
+					//document.getElementById("groupDisplay").innerHTML = "Group Name: "+doc.id+" Module Code: "+doc.data().ModuleCode+" "+ bane;
+					//var evil = '>';
+					//document.getElementById("groupDisplay").innerHTML = "Group Name: "+doc.id+" Module Code: "+doc.data().ModuleCode+' <input type="button" value="Join Group" onClick={ ()=>joinGroup()}>';
+
+					var card = "<div class='row'><div class='col s12 m6'><div class='card blue-grey darken-1'><div class='card-content white-text'><span class='card-title'>"+doc.data().GroupName+"Card Title</span><p>This is where we can add descriptions about the group.</p></div><div class='card-action'><a href='#'>Join Group</a></div></div></div></div>"
+
+					document.getElementById("groupDisplay").innerHTML = card
+				}
+		  })
+		  .catch(err => {
+				document.getElementById("groupDisplay").innerHTML = "Error getting document: "+err;
+		  });
+	}
+}
+
+export const joinGroup = () => {
+	alert("Hello");
+	console.log('Button');
+}
