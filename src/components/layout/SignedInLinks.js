2ae8440135fda076f29ba6514fe6c0883ec7d486
Guoda <33546350+guodap@users.noreply.github.com>
2019-11-24 19:46:46 +0000

    Added links to the navbar buttons

--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -5,8 +5,8 @@ const SignedInLinks = () => {
   return (
     <ul className="right">
       <li><NavLink to="/"></NavLink></li>
-      <li><NavLink to="/">Log out</NavLink></li>
-      <li><NavLink to="/" className="btn btn-floating pink lighten-1">User</NavLink></li>
+      <li><NavLink to="/login">Log out</NavLink></li>
+      <li><NavLink to="/profile" className="btn btn-floating pink lighten-1">User</NavLink></li>
     </ul>
   );
 }
--- src/components/layout/SignedOutLinks.js
+++ src/components/layout/SignedOutLinks.js
@@ -4,8 +4,8 @@ import { NavLink } from 'react-router-dom'
 const SignedOutLinks = () => {
   return (
     <ul className="right">
-      <li><NavLink to="/">Signup</NavLink></li>
-      <li><NavLink to="/">Log in</NavLink></li>
+      <li><NavLink to="/createprofile">Signup</NavLink></li>
+      <li><NavLink to="/login">Log in</NavLink></li>
     </ul>
   );
 }
