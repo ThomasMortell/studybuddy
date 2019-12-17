df94eaeb4f12ea224d0b7c94bfc06b4bd162bb90
georgermarshall <56076696+georgermarshall@users.noreply.github.com>
2019-12-17 21:14:29 +0000

    Added multiple groups to messaging system

--- src/components/diplayMessages.js
+++ src/components/diplayMessages.js
@@ -10,21 +10,23 @@ import moment from 'moment';
 import Planner from  './Planner.js';
 
 var db = firebase.firestore();
-/*
+
 var url = "";
 url = window.location.href;
-var group = "Finally";
+var group = "";
 var checker = false;
 
 for(var i=url.length-1; i>=0&&checker===false; i--){
 
 	if(url.charAt(i)==='/'){
 		checker = true;
+        console.log(group);
 	}
 
 	else if((url.charAt(i)==='0')&&(url.charAt(i-1)==='2')&&(url.charAt(i-2)==='%')){
 		i=i-2;
 		group = ' '+group;
+        console.log(group);
 
 	}
 
@@ -33,14 +35,13 @@ for(var i=url.length-1; i>=0&&checker===false; i--){
 	}
 }
 
-if(group.length>1){
-	group = group.substring(0,1);
-}
-*/
 
+
+
+
+console.log(group);
 var user = firebase.auth().currentUser;
-let GroupCollection = db.collection('groups').doc("Finally").collection('messages');
-var messageCount = GroupCollection.doc('--stats--').valueOf('count');
+
 let messageArray = [];
 let count = 0;
 let mData = "";
@@ -50,18 +51,20 @@ class MessageDisplay extends React.Component {
     items: messageArray,
     hasMore: false,
     messageSender: [],
-    messageContent: []
+    messageContent: [],
+      messageGroup: group
   };
 
   componentDidMount() {
+      let GroupCollection = db.collection('groups').doc(this.state.messageGroup).collection('messages');
+      var messageCount = GroupCollection.doc('--stats--').valueOf('count');
     GroupCollection.get().then(snapshot => {
       snapshot.forEach(doc => {
         messageArray.push(doc.id);
         this.state.messageSender.push(doc.get('Sender'));
         this.state.messageContent.push(doc.get('Message'));
-        //console.log(group);
-		console.log("Run Firsta");
-        console.log(doc.get('Sender'));
+
+
         let check = doc.get('Sender');
         this.setState({items: messageArray});
       });
@@ -80,7 +83,7 @@ class MessageDisplay extends React.Component {
     ////////////////////////////////////////////////////////////////////////////////////////////////////////
     var user = firebase.auth().currentUser;
     const admin = require('firebase-admin');
-    let GroupCollection = db.collection('groups').doc("Finally").collection('messages');
+    let GroupCollection = db.collection('groups').doc(this.state.messageGroup).collection('messages');
 
     let getDoc = GroupCollection.doc('--stats--').get().then(doc => {
       if (!doc.exists) {
@@ -97,10 +100,10 @@ class MessageDisplay extends React.Component {
     return (
 			<div className="container">
 				<div className="row">
-					<div className="col s12 m12 l4">
+					<div className="col l4">
 						<Planner/>
 					</div>
-		      <div className="col s12 m12 l4 message-board">
+		      <div className="col l4 message-board">
 		        <h4>Message Board</h4>
 		        <hr />
 		        <InfiniteScroll
@@ -132,19 +135,24 @@ class MessageDisplay extends React.Component {
 		          </div>
 		        </InfiniteScroll>
 
-            <form className="container message-form" id="sendField">
-              <div className="row">
-                <div className="col s8 m8 l8 left input-field message-send-field">
-                  <input className="" type="text" id="messageField" name="messageField" autoComplete="off"/>
-									<label htmlFor="messageField">Message</label>
-                </div>
-                <div className="col s4 m4 l4 right message-send-btn">
-                  <input className="button" type="button" id="messageField" value="Send" onClick={() => sendMessage()}/>
-                </div>
-              </div>
-            </form>
+		        <div className="page">
+		            <script src="https://www.gstatic.com/firebasejs/3.1.0/firebase-database.js"></script>
+		              <div id="sendField" className="chat-form">
+		                <form className="container message-form" id="sendField">
+		                  <div className="row">
+		                    <div className="col l8 input-field">
+		                      <input className="message-text" type="text" id="messageField" name="messageField" autoComplete="off"/>
+													<label htmlFor="messageField">Message</label>
+		                    </div>
+		                    <div className="col l4">
+		                      <input className="button message-send-btn" type="button" value="Send" onClick={() => sendMessage()}/>
+		                    </div>
+		                  </div>
+		                </form>
+		            </div>
+		        </div>
 		      </div>
-					<div className="col l4 hide-on-medium-and-down">
+					<div className="col l4">
 					</div>
 				</div>
 			</div>
