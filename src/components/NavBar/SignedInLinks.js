bd01adf7a2c3ffc374f742a83210c30b1c9045f7
Guoda <33546350+guodap@users.noreply.github.com>
2019-12-17 15:56:23 +0000

    Side nav functionality fixed on small screen

--- src/components/NavBar/SignedInLinks.js
+++ src/components/NavBar/SignedInLinks.js
@@ -1,30 +1,52 @@
-import React from 'react'
+import React,{ Component } from 'react'
 import { NavLink } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { signout } from '../../store/actions/auth'
+import M from 'materialize-css'
 
 // const handleClick = (e) => {
 //   e.preventDefault();
 //   console.log(this.props)
 // }
 
-const SignedInLinks = (props) => {
-  const { auth } = props;
+class SignedInLinks extends Component{
+//const SignedInLinks = (props) => {
+  //const { auth } = props;
+  //console.log(auth)
+  constructor (props) {
+    super(props)
+  }
 
-  console.log(auth)
+  componentDidMount() {
+    M.AutoInit();
+  }
 
-  return (
+  render (){
+    console.log(this.props)
+    return(
     <div>
-      <a href="#" data-target="mobile-demo" className="right sidenav-trigger"><i className="material-icons">menu</i></a>
       <ul id="nav-mobile" className="right hide-on-med-and-down">
-      <li><NavLink to="/about">About</NavLink></li>
+        <li><NavLink to="/about">About</NavLink></li>
         <li><NavLink to="">Groups</NavLink></li>
         <li><NavLink to="/message">Messages</NavLink></li>
-        <li><a onClick={props.signout} href="/">Log out</a></li>
-        <li><NavLink to="/profile" className="btn btn-floating pink lighten-1">{auth.email.charAt(0)}</NavLink></li>
+        <li><a onClick={this.props.signout} href="/">Log out</a></li>
+        <li><NavLink to="/profile" className="btn btn-floating pink lighten-1">{this.props.auth.email.charAt(0)}</NavLink></li>
       </ul>
+
+      <ul class="sidenav" id="mobile-demo">
+        <li><NavLink to="/profile" className="pink lighten-2">Account: {this.props.auth.email}</NavLink></li>
+        <li><NavLink to="/about">About</NavLink></li>
+        <li><NavLink to="">Groups</NavLink></li>
+        <li><NavLink to="/message">Messages</NavLink></li>
+        <li><a onClick={this.props.signout} href="/">Log out</a></li>
+      </ul>
+
+      <a href="#" data-target="mobile-demo" className="right sidenav-trigger">
+      <i className="material-icons">menu</i>
+      </a>
     </div>
   );
+  }
 }
 
 function mapStateToProps(state){
