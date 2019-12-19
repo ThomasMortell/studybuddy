e8bc7c339ebcb4d7630c0bc5a297374fe17299c2
Conor Hackett <conor.hackett.2018@mumail.ie>
2019-12-19 21:37:05 +0000

    Removed a few warnings from CreateGroup.

--- src/components/Groups/CreateGroup.js
+++ src/components/Groups/CreateGroup.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import firebase from "../../services/firebase.js";
-import Message from "../diplayMessages";
-import {Route, Redirect} from "react-router-dom";
 
 var db = firebase.firestore();
 
